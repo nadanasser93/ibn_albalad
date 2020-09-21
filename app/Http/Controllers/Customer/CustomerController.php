d9a5808ba5a21d359186e500235ebbd1aafdf592
faten1983 <faten30883@gmail.com>
2020-09-21 08:13:56 -0700

    Update CustomerController.php

--- app/Http/Controllers/Customer/CustomerController.php
+++ app/Http/Controllers/Customer/CustomerController.php
@@ -18,30 +18,32 @@ class CustomerController extends Controller
     }
 
     public function profile(){
+        $user=Auth::user();
         if(Auth::user()->userable_type==Customer::class) {
             $customer = $this->customer_service->find(auth()->user()->userable_id);
         }
         else $customer=null;
-        return view('customer.profile', compact('customer'));
+        return view('customer.profile', compact('customer','user'));
     }
     public function store(Request $request)
     {
-
-
         $data  = $request->validate([
             'customer_name'=>['required', 'string', 'max:255'],
             'phone'=>['required'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users']
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:customers']
         ]);
-
-        $customer =$this->customer_service->create($data);
+        if(Auth::user()->userable_type==Customer::class) {
+            $customer = $this->customer_service->create($data);
+        }
+        else
+            $customer = $this->customer_service->update($data);
         if($request->hasFile('image')){
-            $name =  $request->image_file->getClientOriginalName();
-            $extension = $request->image_file->getClientOriginalExtension();
+            $name =  $request->image->getClientOriginalName();
+            $extension = $request->image->getClientOriginalExtension();
             $mdf5 = md5($name.'_'.time()).'.'.$extension;
             $customer->addMediaFromRequest('image')->usingFileName($mdf5)->withResponsiveImages()->toMediaCollection('image');
         }
-        $user=Auth::user();
+        $user=User::find($request->user_id);
 
         $user->userable_type = "App\Models\Customer";
         $user->userable_id = $customer->id;
