f23bf4a9777ef24515ef647e525693d5638f157a
faten1983 <faten30883@gmail.com>
2020-09-22 07:32:03 -0700

    Update Order.php

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,20 +3,70 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
-class Order extends BaseModel
+/**
+ * App\Models\Order
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $subscription_id
+ * @property int $transaction_id
+ * @property string $payment_method
+ * @property string $start_date
+ * @property string $end_date
+ * @property float $amount_excl
+ * @property float $amount_incl
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $deleted_at
+ * @property string|null $payment_id
+ * @property string $payment_status
+ * @property-read \App\Models\Subscription $subscription
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Order newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Order newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Order query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Order whereAmountExcl($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Order whereAmountIncl($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Order whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Order whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Order whereEndDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Order whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Order wherePaymentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Order wherePaymentMethod($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Order wherePaymentStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Order whereStartDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Order whereSubscriptionId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Order whereTransactionId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Order whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Order whereUserId($value)
+ * @mixin \Eloquent
+ */
+class Order extends Model
 {
-    protected $fillable=['order_type','	orderable_id','orderable_type','description','city_id','job_id','home_id'];
-    public function orderable(){
-        return $this->morphTo();
-    }
-    public function city(){
-        return $this->belongsTo(City::class,'city_id');
-    }
-    public function job(){
-        return $this->belongsTo(Company::class,'job_id');
-    }
-    public function home(){
-        return $this->belongsTo(Customer::class,'home_id');
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'subscription_id',
+        'transaction_id',
+        'payment_id',
+        'payment_method',
+        'payment_status',
+        'start_date',
+        'end_date',
+        'amount_excl',
+        'amount_incl',
+    ];
+
+
+
+
+    public function subscription()
+    {
+        return $this->belongsTo(Subscription::class);
     }
 }
