a9e913c8043570d4b160dea09078130b723796eb
faten1983 <faten30883@gmail.com>
2020-09-22 08:00:33 -0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('mollie/methods', 'MollieWebhookController@getPaymentMethods');
 Route::get('cities','Api\CityController@getCities');
 Route::get('city_show/{id}','Api\CityController@show');
 Route::get('companies','Api\CompanyController@getCompanies');
